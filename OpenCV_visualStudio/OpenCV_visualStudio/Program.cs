7bbaed5ae2fd915ca2a8c39deeb96667a826dee5
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-01-11 09:59:50 -0600

    Desarrollo de bases de proyecto

---
+++ OpenCV_visualStudio/OpenCV_visualStudio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCV_visualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
